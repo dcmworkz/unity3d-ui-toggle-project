aea6ffc0db2ec18f86f4dc2e17c02bea05b6ace0
lairinusReborn <lairinus@gmail.com>
2018-06-20 19:57:12 -0700

    initial commit

---
+++ UI Toggle Project/Assets/Lairinus.UI/Editor/UIToggleEditor.cs
@@ -0,0 +1,82 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Lairinus.UI;
+using UnityEngine.UI;
+
+namespace Lairinus.UI.EditorScripts
+{
+    [CustomEditor(typeof(UIToggle))]
+    public class UITransitionerEditor : Editor
+    {
+        private SerializedProperty _disabled = null;
+        private SerializedProperty _isOn = null;
+        private SerializedProperty _toggleOnColor = null;
+        private SerializedProperty _toggleOffColor = null;
+        private SerializedProperty _toggleHoveredColor = null;
+        private SerializedProperty _toggleOnSprite = null;
+        private SerializedProperty _toggleOffSprite = null;
+        private SerializedProperty _toggleHoveredSprite = null;
+        private SerializedProperty _allowHoverEvents = null;
+        private Graphic _targetGraphic = null;
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            RenderInspector();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable()
+        {
+            _disabled = serializedObject.FindProperty(EditorStringContainer.propDisabled);
+            _isOn = serializedObject.FindProperty(EditorStringContainer.propIsOn);
+            _toggleOnColor = serializedObject.FindProperty(EditorStringContainer.propToggleOnColor);
+            _toggleOffColor = serializedObject.FindProperty(EditorStringContainer.propToggleOffColor);
+            _toggleHoveredColor = serializedObject.FindProperty(EditorStringContainer.propToggleHoveredColor);
+            _toggleOnSprite = serializedObject.FindProperty(EditorStringContainer.propToggleOnSprite);
+            _toggleOffSprite = serializedObject.FindProperty(EditorStringContainer.propToggleOffSprite);
+            _toggleHoveredSprite = serializedObject.FindProperty(EditorStringContainer.propToggleHoveredSprite);
+            _allowHoverEvents = serializedObject.FindProperty(EditorStringContainer.propAllowHoverEvents);
+            UIToggle tar = (UIToggle)target;
+            _targetGraphic = tar.gameObject.GetComponent<Graphic>();
+        }
+
+        private void RenderInspector()
+        {
+            EditorGUILayout.PropertyField(_isOn);
+
+            EditorGUILayout.PropertyField(_toggleOffColor);
+            EditorGUILayout.PropertyField(_toggleOnColor); ;
+
+            if (_targetGraphic is Image)
+            {
+                EditorGUILayout.PropertyField(_toggleOnSprite);
+                EditorGUILayout.PropertyField(_toggleOffSprite);
+            }
+
+            EditorGUILayout.PropertyField(_allowHoverEvents);
+            if (_allowHoverEvents.boolValue)
+            {
+                EditorGUILayout.PropertyField(_toggleHoveredColor);
+                EditorGUILayout.PropertyField(_toggleHoveredSprite);
+            }
+
+            // Show Toggle Events
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(_disabled);
+        }
+
+        public class EditorStringContainer
+        {
+            public const string propDisabled = "_disabled";
+            public const string propToggleOnColor = "_toggleIsOnColor";
+            public const string propToggleOffColor = "_toggleIsOffColor";
+            public const string propToggleHoveredColor = "_toggleHoveredColor";
+            public const string propToggleOnSprite = "_toggleIsOnSprite";
+            public const string propToggleOffSprite = "_toggleIsOffSprite";
+            public const string propToggleHoveredSprite = "_toggleHoveredSprite";
+            public const string propAllowHoverEvents = "_allowHoverEvents";
+            public const string propIsOn = "_isOn";
+        }
+    }
+}
---
+++ UI Toggle Project/Assets/Lairinus.UI/Scripts/UIToggle.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Lairinus.UI
+{
+    /// <summary>
+    /// a UIToggle is a custom element that surpasses the functionality of a regular toggle. It adds many necessary features that otherwise would not exist.
+    /// </summary>
+    [RequireComponent(typeof(Graphic))]
+    public class UIToggle : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private bool _disabled = false;
+        [SerializeField] private bool _isOn = false;
+        [SerializeField] private Color _toggleIsOnColor = Color.white;
+        [SerializeField] private Color _toggleIsOffColor = Color.white;
+        [SerializeField] private Color _toggleHoveredColor = Color.white;
+        [SerializeField] private Sprite _toggleIsOnSprite = null;
+        [SerializeField] private Sprite _toggleIsOffSprite = null;
+        [SerializeField] private Sprite _toggleHoveredSprite = null;
+        [SerializeField] private UnityEvent _onToggleFalseHandler = new UnityEvent();
+        [SerializeField] private UnityEvent _onToggleHandler = new UnityEvent();
+        [SerializeField] private UnityEvent _onToggleTrueHandler = new UnityEvent();
+        [SerializeField] private bool _allowHoverEvents = false;
+        private bool _isInitialized = false;
+        public bool disabled { get { return _disabled; } set { _disabled = value; } }
+
+        public bool isOn { get { return _isOn; } }
+        public UnityEvent onToggleFalseHandler { get { return _onToggleFalseHandler; } }
+        public UnityEvent onToggleHandler { get { return _onToggleHandler; } }
+        public UnityEvent onToggleTrueHandler { get { return _onToggleTrueHandler; } }
+        private Graphic _thisGraphic = null;
+        private Image _thisImage = null;
+        private Text _thisText = null;
+        private bool _isHovered = false;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            SetToggledState(!_isOn);
+        }
+
+        public void SetToggledState(bool toggledState)
+        {
+            SetToggledStateInternal(toggledState);
+        }
+
+        private void Awake()
+        {
+            TryInitializeComponents();
+            SetToggledState(_isOn);
+        }
+
+        private void TryInitializeComponents()
+        {
+            if (_isInitialized)
+                return;
+
+            _isInitialized = true;
+            _thisGraphic = GetComponent<Graphic>();
+            if (_thisGraphic is Image)
+                _thisImage = GetComponent<Image>();
+            else if (_thisGraphic is Text)
+                _thisText = GetComponent<Text>();
+        }
+
+        private void SetToggledStateInternal(bool toggledState)
+        {
+            TryInitializeComponents();
+            _isOn = toggledState;
+            if (!_isHovered)
+            {
+                if (_isOn)
+                {
+                    _thisGraphic.color = _toggleIsOnColor;
+                    SetImageSpriteInternal(_toggleIsOnSprite);
+                }
+                else
+                {
+                    _thisGraphic.color = _toggleIsOffColor;
+                    SetImageSpriteInternal(_toggleIsOffSprite);
+                }
+            }
+            else
+            {
+                if (_isHovered && _allowHoverEvents)
+                {
+                    _thisGraphic.color = _toggleHoveredColor;
+                    SetImageSpriteInternal(_toggleHoveredSprite);
+                }
+            }
+
+            // Call the events
+            if (_isOn)
+                _onToggleTrueHandler.Invoke();
+            else
+                _onToggleFalseHandler.Invoke();
+            _onToggleHandler.Invoke();
+        }
+
+        private void SetImageSpriteInternal(Sprite sprite)
+        {
+            if (_thisImage != null)
+            {
+                _thisImage.sprite = sprite;
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData data)
+        {
+            if (_allowHoverEvents)
+            {
+                _isHovered = true;
+                SetToggledStateInternal(_isOn);
+            }
+        }
+
+        public void OnPointerExit(PointerEventData data)
+        {
+            _isHovered = false;
+            SetToggledStateInternal(_isOn);
+        }
+
+        public class UIToggleStyles
+        {
+            public Image image = null;
+            public Color toggleOffColor = Color.white;
+            public Color toggleOnColor = Color.white;
+            public Sprite toggleOffSprite = null;
+            public Sprite toggleOnSprite = null;
+        }
+    }
+}
